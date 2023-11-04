437ccbe8e71eca6f472fae0c1de3764782873afc
Omar Niazi <niazi.omark@gmail.com>
2023-11-04 12:22:06 -0400

    Implement login API

--- users/router.ts
+++ users/router.ts
@@ -13,6 +13,7 @@ router.post('/register', (req, res) => {
 
     if(!body["username"] || !body["password"]){
         res.send({err: "Invalid request"});
+        return;
     }
 
     const escapedUsername = db.escape(body["username"]);
@@ -43,10 +44,45 @@ router.post('/register', (req, res) => {
     
 });
 
-router.post('login', (req, res) => {
+router.post('/login', (req, res) => {
     const body = req.body;
     console.log(req.body);
-    res.send(body);
+
+    if(!body["username"] || !body["password"]){
+        res.send({err: "Invalid request"});
+        return;
+    }
+
+    const escapedUsername = db.escape(body["username"]);
+    const attemptedPassword = body["password"];
+
+    let query = 'SELECT * FROM users WHERE username = ' + escapedUsername;
+    db.query(query, (err, result: any, fields) => {
+        if(err){
+            res.send({err});
+            return;
+        }
+
+        if(result.length == 0){
+            res.send({err: "User not found"});
+            return;
+        }
+
+        const matchedObject = result[0];
+        
+        const computedHash = hashPassword(attemptedPassword, matchedObject.salt);
+        console.log(computedHash);
+        console.log(matchedObject.password_hash);
+        if(computedHash.hash !== matchedObject.password_hash){
+            res.send({err: "Invalid username or password"});
+            return;
+        }
+
+        res.send({
+            user_id: matchedObject.id,
+            username: matchedObject.username
+        });
+    });
 });
 
 export {
